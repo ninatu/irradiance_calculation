9e48a7aa4dd4eb9f7532b0f35b51673db87521b1
Nina Tuluptceva <p.pin.p@yandex.ru>
2018-05-07 00:07:35 +0300

    Add run example

---
+++ src/run.sh
@@ -0,0 +1,10 @@
+python3 compute_irradiance.py \
+    --F 23.2 \
+    --side_plat 40 \
+    --side_light 10 \
+    --angle_light 30 \
+    --n_plat 30 \
+    --n_light 30 \
+    --path_V_lambda ../data/linCIE2008v2e_5.xls \
+    --path_RSPD ../data/D65.xls \
+    --output_dir ../results
